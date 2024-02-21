9a1ddc3ec9be73111321356829d417abe42731ff
Martin Gomez Valle <gomezvallemartin@gmail.com>
2024-02-21 09:13:41 -0300

    Mini correccion

--- 00-Unidades/04_while/TP_While/05_While_Validaciones_rising_btl.py
+++ 00-Unidades/04_while/TP_While/05_While_Validaciones_rising_btl.py
@@ -12,7 +12,7 @@ TP: While_validaciones_rising_btl
 ---
 Enunciado:
 Rising BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada 
-por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
+por ventanas emergentes solamente ((osea que por promp)) (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
 
 Los datos requeridos son los siguientes:
     Apellido
@@ -41,9 +41,8 @@ class App(customtkinter.CTk):
 
         self.label2 = customtkinter.CTkLabel(master=self, text="Estado")
         self.label2.grid(row=2, column=0, padx=20, pady=10)
-        self.combobox_tipo = customtkinter.CTkComboBox(
-            master=self, values=["Soltero/a", "Casado/a", "Divorciado/a", "Viudo/a"])
-        self.combobox_tipo.grid(row=2, column=1, padx=20, pady=10)
+        self.txt_tipo = customtkinter.CTkEntry(master=self)
+        self.txt_tipo.grid(row=2, column=1, padx=20, pady=10)
 
         self.label3 = customtkinter.CTkLabel(master=self, text="Legajo")
         self.label3.grid(row=3, column=0, padx=20, pady=10)
