0bccf5446ba30a44648ef6f1e7f4fe2c73fdc706
Pumiz <gomezvallemartin@gmail.com>
2024-04-16 10:55:11 -0300

    Ejercicio 4 arreglado con validacion

--- 03_ejercicios_extra/ej_practicas/punto_4.py
+++ 03_ejercicios_extra/ej_practicas/punto_4.py
@@ -1,54 +1,26 @@
-def validar_ingresos():
-    # Pide tres datos de distintos tipos, los valida con el parceo y printea si es correcto.
-    
-    numeros_enteros = input("Ingrese un numero entero: ")
-    numeros_enteros = int(numeros_enteros)
-    numeros_flotantes = input("Ingrese un numero decimal: ")
-    numeros_flotantes = float(numeros_flotantes)
-    cadenas = input("Ingrese una cadena: ")
-
-    if type(numeros_enteros) == int:
-        print("\nEl numero ingresado es entero")
-
-    if type(numeros_flotantes) == float:
-        print("El numero flotante ingresado es un decimal")
-
-    if type(cadenas) == str:
-        print("Lo ingresado es una cadena")
-
-validar_ingresos()
-
-
-'''
-
-def get_numero_entero():
-    # Socilicita un numero, lo valida con funcion isdigit() y lo retorna
+def get_validaciones(valor, rango_minimo: float, rango_maximo: float, mensaje: str):
+    # Valida un numero entre dos rangos minimos y maximos
     #
+    #    Argumento:
+    #      valor [None] -> Ingresado por el usuario
+    #      rango_minimo [float] -> valor de validacion minima
+    #      rango_maximo [float] -> valor de validacion maxima
+    #      mensaje [str] -> texto de error
     #    Retorna:
-    #      numero_ingresado -> _description_
-    
-    numero_ingresado = input("Ingrese un numero ")
-    if numero_ingresado.isdigit():
-        numero_ingresado = "El numero ingresado es entero"
-    else:
-        numero_ingresado = "El numero ingresado no es un entero."
-    
-    return numero_ingresado
+    #      retorna -> valor ingresado validado
 
-print(get_numero_entero())
+    while valor < rango_minimo or valor > rango_maximo:
+        valor = input(mensaje)
+        valor = float(valor) #Lo parceo en flotante ya que tambien "funciona" como entero
 
+    return valor
 
-def get_cadena():
-    # Socilicita un numero, lo valida y lo retorna
-    #
-    #    Retorna:
-    #      cadena_ingresadad -> caracteres ingresados
-    
-    cadena_ingresada = input("Ingrese una cadena ")
+edad = int(input("\nIngrese su edad "))
+edad = get_validaciones(edad, 18, 100, "Error, reingrese su edad ")
 
-    if type(cadena_ingresada) == str:
-        print("El valor ingresado es una cadena.")
-    else:
-        print("El valor ingresado no es una cadena.")
+altura = float(input("Ingrese su altura en metros "))
+altura = get_validaciones(altura, 1.0, 2.0, "Error, reingrese su altura ")
 
-get_cadena()'''
+nombre = str(input("Ingrese su nombre "))
+
+print(f"\n{nombre} tiene {edad} años y mide {altura}m")
