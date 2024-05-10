29954665c07ca15935d0d17cfbe74d1655402cd3
Pumiz <gomezvallemartin@gmail.com>
2024-05-10 20:43:03 -0300

    Ejercicios de funciones

---
+++ Programación I/Ejercicios Funciones/punto_3.py
@@ -0,0 +1,11 @@
+def get_cadena():
+    # Socilicita un numero y lo retorna
+    #
+    #    Retorna:
+    #      cadena_ingresadad -> caracteres ingresados
+    
+    cadena_ingresada = input("Ingrese una cadena ")
+
+    return cadena_ingresada
+
+print(f"La cadena ingresada dice: {get_cadena()}")
