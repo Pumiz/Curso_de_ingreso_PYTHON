a97b761f12d31a02a60e9138707eac4472e72678
Pumiz <gomezvallemartin@gmail.com>
2024-05-14 17:10:46 -0300

    Pratica matrices colectivos resuelta

--- Programación I/Practica_matrices_colectivos/Colectivos/funciones.py
+++ Programación I/Practica_matrices_colectivos/Colectivos/funciones.py
@@ -4,3 +4,13 @@ def printear_matriz(matriz, filas, columnas):
             print(f"{matriz [i][j]:<5}", end = " ")
                 
         print("")
+
+def imprimir_matriz_con_texto(matriz, texto_por_columna, texto_por_fila):
+    for texto in texto_por_columna:
+        print(f"\t{texto}", end="\t")
+    print("\n")
+
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(f"{texto_por_fila[i]}",f"{matriz[i][j]:<10}", end="\t")
+        print("\n")
--- Programación I/main_colectivos.py
+++ Programación I/main_colectivos.py
@@ -10,30 +10,37 @@ legajo_choferes = [
 ]
 
 recaudaciones = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
+#  L1,L2,L3
+    [0,0,0], #Coche 1
+    [0,0,0], #Coche 2
+    [0,0,0], #Coche 3
+    [0,0,0], #Coche 4
+    [0,0,0]  #Coche 5
 ]
 
-lista_lineas = [247, 278, 318]
+filas = 5
+columnas = 3
 
-filas = 3
-columnas = 5
+recaudacion_linea_1 = 0
+recaudacion_linea_2 = 0
+recaudacion_linea_3 = 0
 
-for i in range(filas):
-    for j in range(columnas):
+recaudacion_coche_1 = 0
+recaudacion_coche_2 = 0
+recaudacion_coche_3 = 0
+recaudacion_coche_4 = 0
+recaudacion_coche_5 = 0
+
+total_recaudacion = 0
+
+texto_por_columna = ["Linea 1:", "Linea 2:", "Linea 3:"]
+texto_por_fila = ["Coche 1: ", "Coche 2: ", "Coche 3: ", "Coche 4: ", "Coche 5: "]
+
+for i in range(columnas):
+    for j in range(filas):
         legajo_choferes[i][j] = random.randint(10, 1000)
 
-printear_matriz(legajo_choferes, 3, 5)
-
-coches = 0
-linea = 0
-
-recaudacion_lineas = [0,0,0]
-
-recaudacion_coches = [0,0,0,0,0]
+printear_matriz(legajo_choferes, 3, 5) #Printeo la matriz para saber cuales legajos existen
 
 interactuar = False
 
@@ -51,58 +58,41 @@ while interactuar == True:
     system("clear")#   "clear" -> Mac     "cls" -> Windows
     match opcion_seleccionada:
         case 1:
-            linea_existente = False
-            coche_existente = False
-            recaudacion_anterior = 0
-
             linea_trabajada = get_int("Ingrese la linea trabajada: ", "La linea indicada no existe", 1, 3, 3)
             coche_ingresado = get_int("Ingrese el coche trabajado: ", "El numero de coche no existe, reintente", 1, 5, 3)
-            recaudacion = get_int("Ingrese lo recaudado: ", "El numero ingresado tiene que ser mas de 0.", 1, 100000000, 3)
+            recaudacion = get_int("Ingrese lo recaudado: ", "El numero ingresado tiene que ser mas de 0.", 1, 100000000, 3) #Puse de maximo 100000000 como si no hubiera maximo
 
-            recaudacion_anterior = recaudacion
-            for j in range (5):
-                for k in range (3):
-                    recaudaciones[coche_ingresado-1][linea_trabajada-1] += recaudacion_anterior
-            
-            printear_matriz(recaudaciones, 5, 3)
+            recaudaciones[coche_ingresado-1][linea_trabajada-1] += recaudacion
+            print("Planilla cargada con exito!")
 
         case 2:
-            print(f"La recaudacion de las lineas fueron:")
-            for i in range(len(linea)):
-                print(linea[i], end=" --> ")
-                print(recaudacion_lineas[i])
+            print("Las recaudaciones de los coches y lineas son las siguientes: ")
+            imprimir_matriz_con_texto(recaudaciones, texto_por_columna, texto_por_fila)
 
+        case 3:
+            for i in range(filas):
+                recaudacion_linea_1 += recaudaciones[i][0]
+                recaudacion_linea_2 += recaudaciones[i][1]
+                recaudacion_linea_3 += recaudaciones[i][2]
+            print(f"La linea 1, genero: ${recaudacion_linea_1}\nLa linea 2, genero: ${recaudacion_linea_2}\nLa linea 3, genero: ${recaudacion_linea_3}")
 
-"""
-match coches:
-    case 1:
-        recaudacion_coches[coches-1] += recaudacion
-    case 2:
-        recaudacion_coches[coches-1] += recaudacion
-    case 3:
-        recaudacion_coches[coches-1] += recaudacion                            
-    case 4:
-        recaudacion_coches[coches-1] += recaudacion                            
-    case 5:
-        recaudacion_coches[coches-1] += recaudacion
+        case 4:
+            for i in range(columnas):
+                recaudacion_coche_1 += recaudaciones[0][i]
+                recaudacion_coche_2 += recaudaciones[1][i]
+                recaudacion_coche_3 += recaudaciones[2][i]
+                recaudacion_coche_4 += recaudaciones[3][i]
+                recaudacion_coche_5 += recaudaciones[4][i]
+            print(f"El coche 1 genero: ${recaudacion_coche_1}\nEl coche 2 genero: ${recaudacion_coche_2}\nEl coche 3 genero: ${recaudacion_coche_3}\nEl coche 4 genero: ${recaudacion_coche_4}\nEl coche 5 genero: ${recaudacion_coche_5}\n")
 
-match linea:
-    case 1:
-        recaudacion_lineas[linea-1] += recaudacion
-    case 2:
-        recaudacion_lineas[linea-1] += recaudacion
-    case 3:
-        recaudacion_lineas[linea-1] += recaudacion
-else:
-print("No existe la linea o el coche")
-else:
-print("El legajo no existe")
-"""
+        case 5:
+            for i in range(filas):
+                for j in range(columnas):
+                    total_recaudacion += recaudaciones[i][j]
+            print(f"El total de todo lo recaudado es de: ${total_recaudacion}")
 
-
-
-
-
-
+        case 6:
+            print("Gracias por indicar la recaudacion!")
+            interactuar = False
 
 
